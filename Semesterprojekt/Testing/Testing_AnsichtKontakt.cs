ee329b945f138ed8c8c2f3b2d640fbd164d86c34
Jael Koller <jaelkoller@hotmail.com>
2025-08-23 13:28:21 +0200

    Erweiterung Testing_AnsichtKontakt mit 25 generierten Notizen

--- Semesterprojekt/Testing/Testing_AnsichtKontakt.cs
+++ Semesterprojekt/Testing/Testing_AnsichtKontakt.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows.Forms;
 
 namespace Semesterprojekt.Testing
@@ -18,12 +13,49 @@ namespace Semesterprojekt.Testing
         {
             InitializeComponent();
         }
+        
+        // Erstellung von 25 Notizen für Test-Kontakt
+        private void CreateTestNotes (string contactNumber)
+        {
+            // Bereinigung allfälliger bestehender Notizen zu Test-Kontakt
+            Notes.DeleteNotesData(contactNumber);
+
+            int countNotes = 25;
+            int daySpacing = 400;
+            DateTime startDate = DateTime.Today.AddDays(-(countNotes - 1) * daySpacing);
+
+            for (int note = 0; note < countNotes; note++)
+            {
+                InitializationNotes noteData = new InitializationNotes
+                {
+                    ContactNumber = contactNumber,
+                    NoteTitle = $"Das ist der Titel zu einer Testnotiz! #{countNotes - note}",
+                    NoteText = $"Das ist der Text zu der Testnotiz #{countNotes - note}. Hier werden nun ein paar Punkte festgehalten:\r\n\r\n" +
+                    $"- Erfassung von Mitarbeitern und Kunden funktioniert\r\n" +
+                    $"- Mutieren von Mitarbeitern und Kunden funktioniert\r\n" +
+                    $"- Aktivieren und Deaktivieren von Mitarbeitern und Kunden funktioniert\r\n" +
+                    $"- Löschen von Mitarbeitern und Kunden funktioniert\r\n" +
+                    $"- Automatische Vergabe von Mitarbeiternummern (ebenso Kundennummern) funktioniert\r\n" +
+                    $"- Protokollieren von Notizen in Kundenkontakten inkl. Historie funktioniert\r\n" +
+                    $"- Suchmöglichkeiten über gespeicherten Informationen (Name, Vorname, Geburtsdatum, Mitarbeiter/Kunde) funktioniert\r\n" +
+                    $"- Automatisches Speichern und Laden des Datenstamms auf Festplatte funktioniert\r\n\r\n" +
+                    $"Es gibt noch diverse optionale Anforderungen, wie z.B. Validierungslogiken usw., welche den Benutzer unterstützen.",
+                    NoteDate = startDate.AddDays(note * daySpacing).ToString("dd.MM.yyyy", CultureInfo.InvariantCulture)
+                };
+
+                Notes.SaveNotesData(noteData);
+            }
+        }
+        
         public void TestData(bool testfallMitarbeiter)
         {
             string contactStatus = testfallMitarbeiter ? "active" : "inactive";
-            string contactNumber = testfallMitarbeiter ? "MA1717" : "KD1717";
+            string contactNumber = testfallMitarbeiter ? "MA-Test" : "KD-Test";
             string typeOfContact = testfallMitarbeiter ? "Mitarbeiter" : "Kunde";
 
+            // Aufruf Erstellung Notizen
+            CreateTestNotes(contactNumber);
+            
             // Erstellung Testdaten            
             var contactData = new InitializationContactData
             {
@@ -44,7 +76,7 @@ namespace Semesterprojekt.Testing
                     { "BusinessNumber", "071 123 45 67" },
                     { "MobileNumber", "076 123 45 67" },
                     { "Email", "jaelkoller@testmail.ch" },
-                    { "EmployeeNumber", testfallMitarbeiter ? "MA1717" : "" },
+                    { "EmployeeNumber", testfallMitarbeiter ? "MA9999" : "" },
                     { "AHVNumber", testfallMitarbeiter ? "756.8800.5641.37" : "" },
                     { "Nationality", testfallMitarbeiter ? "CH" : "" },
                     { "ManagementLevel", testfallMitarbeiter ? "Mitglied der Direktion" : "" },
